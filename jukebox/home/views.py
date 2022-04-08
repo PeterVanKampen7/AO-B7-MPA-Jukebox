7501675a1644376d8869c6a23ee0a5bd7b2ed296
Peter <99065921@mydavinci.nl>
2022-04-08 13:29:44 +0200

    Changes to landing page

--- jukebox/home/views.py
+++ jukebox/home/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
 
     context = {
-        
+        'page_title': 'Home',
     }
 
     return render(request, 'home.html', context)
