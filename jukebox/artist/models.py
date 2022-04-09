23645b5712cf45439940cf8378b2cc9ff556ecf8
PeterVanKampen7 <71004497+PeterVanKampen7@users.noreply.github.com>
2022-04-09 23:59:37 +0200

    Artist template pages

--- jukebox/artist/models.py
+++ jukebox/artist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    age = models.IntegerField()
+    nationality = models.CharField(max_length=30)
+
+    def get_absolute_url(self):
+        return reverse("artist_detail", kwargs={"artist_id": self.id})
