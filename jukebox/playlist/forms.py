5c29a0b1eef1b1bd0527773c9deb515b138aa5c4
Peter <99065921@mydavinci.nl>
2022-05-11 12:08:39 +0200

    Comments for Playlist Forms and Model

--- jukebox/playlist/forms.py
+++ jukebox/playlist/forms.py
@@ -3,6 +3,8 @@ from .models import Playlist
 
 class PlaylistForm(forms.ModelForm):
 
+    # Model form for editing and creating playlist, containing the fields name and songs
+
     class Meta:
         model = Playlist
         fields = [
--- jukebox/playlist/models.py
+++ jukebox/playlist/models.py
@@ -4,19 +4,27 @@ from song.models import Song
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Playlist(models.Model):
+    # Model for a playlist
+
+    # Name of the playlist, max length of 30 characters
     name = models.CharField(max_length=30)
-
+    # Assign the playlist to a user
+    # When playlist is deleted do not delete user
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
-
+    # Add many to many Song relation
     songs = models.ManyToManyField(Song)
 
+    # This function returns the url leading to this playlist
     def get_absolute_url(self):
         return reverse("playlist_detail", kwargs={"playlist_id": self.id})
 
+    # When this object is called output the playlist name
     def __str__(self):
         return self.name
 
+    # Set user for this playlist, user is passed as parameter
     def set_user(self, user):
         self.user = user
-        self.save()
+        self.save() 
