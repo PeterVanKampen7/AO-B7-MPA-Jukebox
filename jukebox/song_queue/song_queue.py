71a146a1b761e2030f30f02cbee9bd901652eb36
Peter <99065921@mydavinci.nl>
2022-05-13 09:46:28 +0200

    Comments for Song_queue app

--- jukebox/song_queue/song_queue.py
+++ jukebox/song_queue/song_queue.py
@@ -3,55 +3,84 @@ from song.models import Song
 from playlist.models import Playlist
 
 class SongQueue():
+    # This class handles all interaction with the SESSION
+    # (SESSION in Django is in fact a COOKIE)
 
     @staticmethod
+    # This method is static and adds a song to the queue
     def addSong(request):
+        # Get the song with the id passed through the submitted form
         song = get_object_or_404(Song, id=request.POST.get('song_id',''))
 
+        # Try to add the song into the queue
         try:
             request.session['queue'].append(song.id)
+        # If this fails the queue does not exist yet
+        # Make the queue first and then add the song to it
         except:
             request.session['queue'] = []
+            # This index tracks what song in the queue is currently being 'played'
             request.session['queue_index'] = 0
             request.session['queue'].append(song.id)
             
+        # Indicate to Django the Session has been modified so that the changes will be saved
         request.session.modified = True
 
     @staticmethod
+    # This method is static and moves the queue to the next song
     def nextSong(request):
+        # If the current index is not already at the last song moves the index up by one
         if len(request.session['queue']) - 1 > request.session['queue_index']:
             request.session['queue_index'] += 1
 
+            # Indicate to Django the Session has been modified so that the changes will be saved
             request.session.modified = True
 
     @staticmethod
+    # This method is static and moves the queue to the previous song
     def prevSong(request):
+        # If the queue is not already at the first song decrease the index by one
         if 0 < request.session['queue_index']:
             request.session['queue_index'] -= 1
 
+            # Indicate to Django the Session has been modified so that the changes will be saved
             request.session.modified = True
 
     @staticmethod
+    # This method is static and clears the entire queue
     def clearQueue(request):
+        # Create a new empty queue and set the index to 0
         request.session['queue'] = []
         request.session['queue_index'] = 0
 
+        # Indicate to Django the Session has been modified so that the changes will be saved
         request.session.modified = True
 
     @staticmethod
+    # This method is static and saves the queue as a Playlist
     def saveAsPlaylist(song_id_list, playlist_name, user):
+        # Create a new playlist with the name passed as parameter
         new = Playlist(name = playlist_name)
+        # Add the current user as owner of this new Playlist
         new.user = user
+        # Save the new playlist. Without saving the foreign key relations cannot be added
         new.save()
 
+        # For each song in the queue add it to the new Playlist
         for song_id in song_id_list:
             new.songs.add(get_object_or_404(Song, id = song_id))
+
+        # Save the Playlist
         new.save()
 
+        # Return the Playlist
         return new
 
     @staticmethod
+    # This method removes a song from the queueu, indicated by an index
     def removeFromQueue(request, index):
+        # Remove the song from the queue at the index passed as parameter
         request.session['queue'].pop(int(index))
 
+        # Indicate to Django the Session has been modified so that the changes will be saved
         request.session.modified = True
--- jukebox/song_queue/views.py
+++ jukebox/song_queue/views.py
@@ -3,37 +3,54 @@ from song.models import Song
 from song_queue.song_queue import SongQueue
 
 # Create your views here.
+
 def queue_detail(request):
+    # Create view for the queue detail page
+
+    # If user has pressed the next song button, run the method in the song_queue Class
     if request.POST.get('next_song',''):
         SongQueue.nextSong(request)
 
+    # If user has pressed the previous song button, run the method in the song_queue Class
     if request.POST.get('previous_song',''):
         SongQueue.prevSong(request)
 
+    # If user has pressed the clear queue button, run the method in the song_queue Class
     if request.POST.get('clear_queue',''):
         SongQueue.clearQueue(request)
 
+    # If user has pressed the save queue button, run the method in the song_queue Class
     if request.POST.get('save_queue',''):
+        # pass the name and current user as paramenters
         playlist = SongQueue.saveAsPlaylist(request.session['queue'], request.POST.get('new_playlist_name', ''), request.user)
+        # Redirect to the Playlist that was just made
         return redirect(f'/playlist/{playlist.id}')
 
+    # If user has pressed the delete song button, run the method in the song_queue Class
     if request.GET.get('delete_entry', ''):
         SongQueue.removeFromQueue(request, request.GET.get('delete_entry', ''))
+        # Redirect to queue detail page
         return redirect('/song_queue')
         
 
+    # These oprations will fail if no queue exists yet
     try:
+        # Get the queue filled with song id's
         song_id_list = request.session['queue'] or None
         song_list = []
     
+        # Fill array with Song objects, retrieved with the song id's from the queue
         for song_id in song_id_list:
             song_list.append(get_object_or_404(Song, id=song_id))
 
+        # Set a variable with the current song
         current_song_index = request.session['queue_index']
         current_song = song_list[current_song_index]  
     except:
+        # If no queue exists redirect to Home page
         return redirect('/')
 
+    # Pass page name, queue, current song and current song index into context
     context = {
         'page_title': 'Song queue',
         'queue': song_list,
@@ -41,4 +58,5 @@ def queue_detail(request):
         'current_song_index': current_song_index,
     }
 
+    # Render the template
     return render(request, 'song_queue/queue_detail.html', context)
