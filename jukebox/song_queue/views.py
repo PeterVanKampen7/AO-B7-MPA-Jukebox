2c55522ed8d1b6c81057e042ab1183f44a78c9c8
Peter <99065921@mydavinci.nl>
2022-05-13 09:57:42 +0200

    Show total duration in queue

--- jukebox/song_queue/views.py
+++ jukebox/song_queue/views.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.shortcuts import get_object_or_404, redirect, render
 from song.models import Song
 from song_queue.song_queue import SongQueue
@@ -38,10 +39,15 @@ def queue_detail(request):
         # Get the queue filled with song id's
         song_id_list = request.session['queue'] or None
         song_list = []
+
+        # Create empty timedelta to track total duration of the queue
+        queue_duration = timedelta()
     
         # Fill array with Song objects, retrieved with the song id's from the queue
         for song_id in song_id_list:
-            song_list.append(get_object_or_404(Song, id=song_id))
+            song = get_object_or_404(Song, id=song_id)
+            song_list.append(song)
+            queue_duration += song.duration
 
         # Set a variable with the current song
         current_song_index = request.session['queue_index']
@@ -50,10 +56,11 @@ def queue_detail(request):
         # If no queue exists redirect to Home page
         return redirect('/')
 
-    # Pass page name, queue, current song and current song index into context
+    # Pass page name, queue, queue duration, current song and current song index into context
     context = {
         'page_title': 'Song queue',
         'queue': song_list,
+        'queue_duration': queue_duration,
         'current_song': current_song,
         'current_song_index': current_song_index,
     }
